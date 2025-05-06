4a5cfe45f7f25e038b49053c327e42c5ba29b878
Sawath <syllasawath28@gmail.com>
2025-05-06 14:35:44 +0200

    Final

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,10 @@
 import os
 import sys
 from fastapi.testclient import TestClient
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from main import app
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 client = TestClient(app)
 
